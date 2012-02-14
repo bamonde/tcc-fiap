225829fa050de211e80b58562213f3afd265900d
Ubuntu <ubuntu@ip-10-252-19-182.sa-east-1.compute.internal>
2012-02-14 15:27:16 +0000

    Arquivo bash para criar estrutura de pastas temporarias.

---
+++ criar_estrutura_tmp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p /var/www/tcc/app/tmp/cache/models
+mkdir -p /var/www/tcc/app/tmp/cache/persistent
+mkdir -p /var/www/tcc/app/tmp/cache/css
+mkdir -p /var/www/tcc/app/tmp/cache/views
+mkdir -p /var/www/tcc/app/tmp/logs
+mkdir -p /var/www/tcc/app/tmp/sessions
+mkdir -p /var/www/tcc/app/tmp/tests
+chmod 777 -R /var/www/tcc/app/tmp/
+
